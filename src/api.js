318976bc6ee0b2a4af6a11677a79fc7c3d618d27
weshy <josephwaweru96@gmail.com>
2022-07-16 20:00:27 +0300

    search componet

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4f243cda16mshf01ee2a2e2dcafep1bb9c5jsn638f69497598',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+
