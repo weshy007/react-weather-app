c004e3159b139c0022391b8953bce6d5f9973629
weshy <josephwaweru96@gmail.com>
2022-07-16 20:47:10 +0300

    current weather component

--- src/api.js
+++ src/api.js
@@ -8,3 +8,6 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "8a17cdd2517228ad7b3b3891eb3118ac";
